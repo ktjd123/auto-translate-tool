c8e93cf7bfadc6191dcf648b30f5c8570d74bd5c
ktjd1262 <k9326239@gmail.com>
2018-05-29 16:57:27 +0900

    Initial Commit

---
+++ auto-translate-tool/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  auto-translate-tool
+//
+//  Created by 김성민 on 2018. 5. 29..
+//  Copyright © 2018년 김성민. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
